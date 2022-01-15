cef92e724431c2886dc4c5b7c59e5c698c9fe3a5
ktundu <78655407+chimbuzi@users.noreply.github.com>
2022-01-15 12:23:01 +0000

    Update find_disk.sh
    Added a bunch of functionality

--- find_disk.sh
+++ find_disk.sh
@@ -13,25 +13,87 @@ then
         exit 1
 fi
 
-TARGETWWN=$1
+# figure out if we've been given a wwn
+if [[ "$1" == "wwn"* ]]
+then
+        # do something
+        echo "Looking up by WWN..."
+        TARGETWWN=$1
 
-# resolve the actual device file pointed to by the wwm symlink
-TARGETDEV=$(readlink -f /dev/disk/by-id/$TARGETWWN)
+        # resolve the device file pointed to by the symlink
+        TARGETDEV=$(readlink -f /dev/disk/by-id/$TARGETWWN)
+        echo $TARGETDEV
+        # list all devices by ID and loop over then
+        ALLDEV="/dev/disk/by-id/*"
+        for DEV in $ALLDEV
+        do
+                # check if we've just matched the WWN we passed in the script. If so, keep looking.
+                if [ $DEV == "wwn*" ]
+                then
+                        continue
+                fi
+                # match the symlink target to the target from the passed WWN. If match, return serial.
+                if [ $(readlink -f $DEV) == $TARGETDEV ]
+                then
+                        echo $DEV
+                        break
+                fi
+        done
+        exit 0
 
-# list all devices by ID and loop over them
+fi
+
+# figure out if we've been given a device name
+if [[ "$1" == "sd"* ]] || [[ "$1" == "/dev/sd"* ]]
+then
+        # do something
+        echo "Looking up by device name..."
+
+        # find the right device file regardless of what was passed
+        ALLDEV="/dev/*"
+        for DEV in $ALLDEV
+        do
+                if [[ "$DEV" == *"$1"* ]]
+                then
+                        TARGETDEV=$DEV
+                fi
+        done
+
+        # then look up the WWN and serial
+        ALLDEV="/dev/disk/by-id/*"
+        for DEV in $ALLDEV
+        do
+                if [ $(readlink -f $DEV) == $TARGETDEV ]
+                then
+                        echo $DEV
+                fi
+        done
+
+
+
+
+
+        exit 0
+fi
+
+
+
+# otherwise, assume serial number
+echo "Looking up by serial number..."
+TARGETSERIAL=$1
+TARGETDEV=$(readlink -f /dev/disk/by-id/$TARGETSERIAL)
+echo $TARGETDEV
 ALLDEV="/dev/disk/by-id/*"
 for DEV in $ALLDEV
 do
-        # check if we've just matched the WWN we passed the script. If so, keep looking.
-        if [ $DEV == "wwn*" ]
+        if [[ "$DEV" != *"wwn"* ]]
         then
                 continue
         fi
-        # Match the symlink target to the target from the passed WWN. If match, return serial.
         if [ $(readlink -f $DEV) == $TARGETDEV ]
         then
                 echo $DEV
                 break
         fi
-
 done
+exit 0
